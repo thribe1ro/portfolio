c6f36f151c7abb31e8df189be9dddd3a039c47cb
Thiago Ribeiro <thifelipe2011@gmail.com>
2024-12-03 10:32:35 -0300

    Att

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -3,3 +3,20 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+
+  var curriculo = document.getElementById("openCurriculo")
+
+  curriculo.addEventListener("click", function() {
+    var openCurriculo = document.getElementById("curriculo")
+    let x = document.getElementById("x")
+    var abrirCurriculo = false
+
+    abrirCurriculo = !abrirCurriculo
+    openCurriculo.style.display = abrirCurriculo ? "flex" : "none"
+
+    x.addEventListener("click", function() {
+        abrirCurriculo = !abrirCurriculo
+        openCurriculo.style.display = abrirCurriculo ? "flex" : "none"
+    })
+  })
